b20f9e53613585a3912bbf045a2a97304ffc860b
Rob Kayson <rkayson@gmail.com>
2019-09-29 09:00:24 -0700

    First Game! Collect green dots. Poor performance.

--- scripts/Circle.gd
+++ scripts/Circle.gd
@@ -6,6 +6,7 @@ var debug_properties: Array = [
 	'mass',
 	'drag',
 	'release_factor',
+	'max_speed',
 #	'acceleration',
 #	'velocity',
 #	'release_factor',
@@ -17,19 +18,58 @@ var debug_properties: Array = [
 #	'position',
 ]
 
+export var is_player:bool = false
+export var is_ball:bool = false
+export var is_collectible:bool = false setget is_collectible_set, is_collectible_get
+export var can_collect:bool = false
 export var grabbable:bool = true
-export var color:String = '#ff0000'
+export var color:String = '#ff0000' setget color_set
 export var radius:float = 100.0 setget radius_set
 export var mass:float = 100.0 setget mass_set
 export var drag:float = 0.8 setget drag_set
 export var release_factor:float = 40.0 setget release_factor_set
 export var acceleration:Vector2 = Vector2()
-export var velocity:Vector2 = Vector2()
+export var velocity:Vector2 = Vector2() setget velocity_set
+export var max_speed:float = 3000 setget max_speed_set
 var grabbed:bool = false
-var grabbed_offset:Vector2 = Vector2()
+var grabbed_offset:Vector2 = Vector2() setget grabbed_offset_set
 var finger_position:Vector2 = Vector2()
 var touch_index:int = -1
 var last_position:Vector2 = Vector2()
+
+func color_set(new_color:String) -> void:
+	color = new_color
+	update()
+
+func ball_collide(circle:Circle) -> void:
+	if is_player and circle.is_ball:
+		var ball:Circle = circle
+		ball.color = color
+		ball.can_collect = true
+
+func grabbed_offset_set(new_grabbed_offset:Vector2) -> void:
+	grabbed_offset = new_grabbed_offset
+	if position.distance_to(grabbed_offset) > radius:
+		#release
+		grabbed = false
+		touch_index = -1
+		
+func velocity_set(new_velocity: Vector2) -> void:
+	velocity = new_velocity
+	if velocity.length() > max_speed:
+		velocity = velocity.clamped(max_speed)
+
+func is_collectible_set(collectible:bool) -> void:
+	is_collectible = collectible
+
+func is_collectible_get() -> bool:
+	return is_collectible and visible
+
+func collect(circle:Circle) -> bool:
+	if can_collect and circle.is_collectible:
+		circle.hide()
+		return true
+	return false
 
 func radius_set(new_radius:float) -> void:
 	radius = new_radius
@@ -43,6 +83,9 @@ func drag_set(new_drag:float) -> void:
 
 func release_factor_set(new_release_factor:float) -> void:
 	release_factor = new_release_factor
+
+func max_speed_set(new_max_speed:float) -> void:
+	max_speed = new_max_speed
 	
 func _ready():
 	pass
@@ -55,6 +98,9 @@ func _process(delta):
 	
 func is_within_circle(point:Vector2) -> bool:
 	return position.distance_to(point) <= radius
+
+func collect_if_possible() -> void:
+	hide()
 	
 func grab_if_possible(event:InputEventScreenTouch) -> void:
 	if not grabbable or grabbed:
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,44 +2,107 @@ extends Node2D
 class_name Game
 
 onready var screen_size = get_viewport_rect().size
-var rng = RandomNumberGenerator.new()
 
 var velocity_clamp = 10
 var circles = []
 
 signal created_circle
 
+var red_score = 0
+var blue_score = 0
+var ball
+
 func _ready():
 	randomize()
-	rng.randomize()
-	# Small Red
+	# Small Blue
 	create_circle({
-		'position': Vector2(screen_size.x * 0.5, 500),
+		'position': Vector2(screen_size.x * 0.5, 300),
+		'radius': 70,
+		'drag': 1,
+		'release_factor': 40,
+		'color': '#0000ff',
+		'is_player': true,
 	})
 	# Small Red
 	create_circle({
-		'position': Vector2(screen_size.x * 0.5, screen_size.y - 500),
+		'position': Vector2(screen_size.x * 0.5, screen_size.y - 300),
+		'radius': 70,
+		'drag': 1,
+		'release_factor': 40,
+		'is_player': true,
 	})
 	# White Ball
-	create_circle({
+	ball = create_circle({
 		'grabbable': false,
 		'position': screen_size * 0.5,
 		'color': '#ffffff',
-		'radius': 50,
-		'mass': 50,
+		'radius': 100,
+		'mass': 100,
+		'can_collect': false,
+		'is_ball': true,
 	})
 	# Big Blue
-	create_circle({
-		'position': Vector2(randf() * screen_size.x, randf() * screen_size.y),
-		'color': '#0000ff',
-		'radius': 300,
-		'mass': 600,
-		'drag': 1,
-		'release_factor': 10,
-	})
-#	for i in range(1):
-#		create_circle()
+#	create_circle({
+#		'position': Vector2(randf() * screen_size.x, randf() * screen_size.y),
+#		'color': '#0000ff',
+#		'radius': 300,
+#		'mass': 600,
+#		'drag': 1,
+#		'release_factor': 10,
+#		'collectible': true,
+#	})
+	var margin_x = 400
+	var margin_y = 400
+	var rows = 8
+	var cols = 5
+	var x = margin_x
+	var y = margin_y
+	var i = 0
+	while x < screen_size.x - margin_x:
+		while y < screen_size.y - margin_y:
+			create_circle({
+				'position': Vector2(x,y),
+				'radius': 30,
+				'color': '#00ff00',
+				'is_collectible': true,
+				'mass': 200,
+			})
+			i += 1
+			print(i)
+			y += screen_size.y / rows
+		x += screen_size.x / cols
+		y = margin_y
+		
+	
+	# Blurp
+#	for i in range(50):
+#		create_circle({
+#			'position': Vector2(400,400),
+#			'radius': 30,
+#			'color': '#00ff00',
+#			'is_collectible': true,
+#		})
+	
+# Corners!
+#	var margin = 50
+#	var rows = 20
+#	var cols = 10
+#	var x = 0
+#	var y = 0
+#	while x < screen_size.x:
+#		while y < screen_size.y:
+#			create_circle({
+#				'position': Vector2(margin + screen_size.x / (x+1), margin + screen_size.y / (y+1)),
+#				'radius': 30,
+#				'color': '#00ff00',
+#				'is_collectible': true,
+#			})
+#			y += 250
+#		x += 250
+#		y = 0
 
+func get_random_position() -> Vector2:
+	return Vector2(randf() * screen_size.x, randf() * screen_size.y)
 
 func _unhandled_input(event):
 	if event is InputEventScreenTouch:
@@ -62,7 +125,7 @@ func _physics_process(delta):
 	handle_circle_logic(delta)
 	pass
 	
-	
+var top_speed = Vector2()
 func handle_circle_logic(delta):
 	var colliding_pairs = []
 	
@@ -74,6 +137,9 @@ func handle_circle_logic(delta):
 			var velocity_on_last_frame = (c.position - last_position) * c.release_factor
 			c.velocity = (c.velocity + velocity_on_last_frame) / 2
 		else:
+			if c.velocity.length() > top_speed.length():
+				top_speed = c.velocity
+				print(top_speed.length())
 			c.acceleration = -c.velocity * c.drag
 			c.velocity += c.acceleration * delta
 			c.position += c.velocity * delta
@@ -90,7 +156,6 @@ func handle_circle_logic(delta):
 
 			# Bounce off screen edge
 			# NOTE: changing velocity here feels like no-no
-			var wall_circle
 			if c.position.x - c.radius < 0:
 				c.position.x = c.radius
 				c.velocity.x = -c.velocity.x
@@ -110,21 +175,45 @@ func handle_circle_logic(delta):
 		
 	# Static collision ie overlap
 	for c1 in circles:
+		if c1.velocity.x == 0 and c1.velocity.y == 0:
+			continue
 		for c2 in circles:
-			if c1.name != c2.name:
+			# Visibility stuff is temp for collectibles (maybe?)
+			if c1.name != c2.name and c1.visible and c2.visible:
 				var distance_vector = c1.position - c2.position
 				var distance_between_circles = distance_vector.length()
 				var radii = c1.radius + c2.radius
 				var circles_overlap = distance_between_circles < radii
 				if circles_overlap:
-					colliding_pairs.push_back([c1, c2])
+					# Ball stuff
+					c1.ball_collide(c2)
+					
+					# Collectibles
+					var collected:bool = c1.collect(c2)
+					if collected:
+						if ball.color == '#ff0000':
+							red_score += 1
+							$CanvasLayer/RedLabel.text = String(red_score)
+						if ball.color == '#0000ff':
+							blue_score += 1
+							$CanvasLayer/BlueLabel.text = String(blue_score)
+						continue
+					
+					if not c1.grabbed and not c2.grabbed:
+						colliding_pairs.push_back([c1, c2])
 					
 					# Displace balls away from collision
 					var overlap = 0.5 * (distance_between_circles - c1.radius - c2.radius)
 					var displacement = overlap * distance_vector / distance_between_circles
 					# BUG: adding 1px displacement for c1 because `distance_between_circles < radii` evaluates to true even when the values are equal..
-					c1.position -= displacement + displacement.normalized() 
-					c2.position += displacement
+					if c2.grabbed:
+						c2.grabbed_offset -= displacement
+					else:
+						c1.position -= displacement + displacement.normalized() 
+					if c1.grabbed:
+						c1.grabbed_offset -= displacement
+					else:
+						c2.position += displacement
 		
 	# Dynamic Collision
 	for pair in colliding_pairs:
@@ -192,11 +281,6 @@ func _on_BottomGoal_body_entered(body):
 		$Ball.position = get_viewport_rect().size * 0.5
 		$Ball.velocity = Vector2()
 
-
-
-func get_random_position():
-	return Vector2(rng.randi_range(0, screen_size.x), rng.randi_range(0, screen_size.y))
-
 func new_circle(props:Dictionary = {}) -> Circle:
 	var circle = load('res://scenes/Circle.tscn').instance()
 	for key in props.keys():
@@ -204,8 +288,9 @@ func new_circle(props:Dictionary = {}) -> Circle:
 		circle.set(key, val)
 	return circle
 
-func create_circle(props:Dictionary = {}):
+func create_circle(props:Dictionary = {}) -> Circle:
 	var circle:Circle = new_circle(props)
 	add_child(circle)
 	circles.push_back(circle)
 	emit_signal("created_circle", circle)
+	return circle
